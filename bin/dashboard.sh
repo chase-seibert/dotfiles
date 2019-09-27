c419d68cdf968a0db1533e16a8fc8b2bf4c973bc
Chase Seibert <cseibert@dropbox.com>
2019-09-27 11:48:20 -0700

    Re-order dashboard windows

--- bin/dashboard.sh
+++ bin/dashboard.sh
@@ -11,16 +11,20 @@ tmux new-session -d -s $session
 
 tmux select-window -t $session:0
 tmux rename-window jira-carry-over
-tmux send-keys "~/projects/jira-carry-over/.virtualenv/bin/python ~/projects/jira-carry-over/jira-carry-over.py report --board 446 --since 2019-01-01" C-m
+tmux send-keys "~/projects/jira-carry-over/.virtualenv/bin/python ~/projects/jira-carry-over/jira-carry-over.py report --board 446 --since 2019-07-01" C-m
 
 # create a new window called diffs
 tmux new-window -t $session:2 -n code-review
 tmux select-window -t $session:2
 tmux send-keys "cd /Users/cseibert/projects/differential-comments; ./.virtualenv/bin/python differential-comments.py --days 10 --comment-days 1" C-m
 
-tmux new-window -t $session:3 -n git-stats
+tmux new-window -t $session:3 -n jira-comments
 tmux select-window -t $session:3
-tmux send-keys "cd /Users/cseibert/src/server; gf origin; cd /Users/cseibert/projects/git-stats; python git-stats.py --path /Users/cseibert/src/server --days 30 --just-totals" C-m
+tmux send-keys "~/projects/jira-comments/.virtualenv/bin/python ~/projects/jira-comments/jira-comments.py list --board 446 --days 3" C-m
+
+tmux new-window -t $session:4 -n git-stats
+tmux select-window -t $session:4
+tmux send-keys "cd /Users/cseibert/src/server; gf origin; cd /Users/cseibert/projects/git-stats; ./.venv/bin/python git-stats.py --path /Users/cseibert/src/server --days 90 --just-totals" C-m
 
 # return to main vim window
 tmux select-window -t $session:0
