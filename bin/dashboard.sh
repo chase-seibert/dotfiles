058687146a9c91eb3448e1eee1a757d713c4df1f
Chase Seibert <cseibert@dropbox.com>
2019-10-29 08:53:26 -0700

    Broke out JIRA for four teams

--- bin/dashboard.sh
+++ bin/dashboard.sh
@@ -10,20 +10,31 @@ tmux start-server
 tmux new-session -d -s $session
 
 tmux select-window -t $session:0
-tmux rename-window jira-carry-over
+tmux rename-window jira-sh-spaces
 tmux send-keys "~/projects/jira-carry-over/.virtualenv/bin/python ~/projects/jira-carry-over/jira-carry-over.py report --board 446 --since 2019-07-01" C-m
 
-# create a new window called diffs
-tmux new-window -t $session:2 -n code-review
+tmux new-window -t $session:2 -n jira-sh-exp
 tmux select-window -t $session:2
+tmux send-keys "~/projects/jira-carry-over/.virtualenv/bin/python ~/projects/jira-carry-over/jira-carry-over.py report --board 144 --since 2019-07-01" C-m
+
+tmux new-window -t $session:3 -n jire-sh-found
+tmux select-window -t $session:3
+tmux send-keys "~/projects/jira-carry-over/.virtualenv/bin/python ~/projects/jira-carry-over/jira-carry-over.py report --board 794 --since 2019-07-01" C-m
+
+tmux new-window -t $session:4 -n jira-ts
+tmux select-window -t $session:4
+tmux send-keys "~/projects/jira-carry-over/.virtualenv/bin/python ~/projects/jira-carry-over/jira-carry-over.py report --board 839 --since 2019-07-01" C-m
+
+tmux new-window -t $session:5 -n code-review
+tmux select-window -t $session:5
 tmux send-keys "cd /Users/cseibert/projects/differential-comments; ./.virtualenv/bin/python differential-comments.py --days 10 --comment-days 1" C-m
 
-tmux new-window -t $session:3 -n jira-comments
-tmux select-window -t $session:3
+tmux new-window -t $session:6 -n jira-comments
+tmux select-window -t $session:6
 tmux send-keys "~/projects/jira-comments/.virtualenv/bin/python ~/projects/jira-comments/jira-comments.py list --board 446 --days 3" C-m
 
-tmux new-window -t $session:4 -n git-stats
-tmux select-window -t $session:4
+tmux new-window -t $session:7 -n git-stats
+tmux select-window -t $session:7
 tmux send-keys "cd /Users/cseibert/src/server; gf origin; cd /Users/cseibert/projects/git-stats; ./.venv/bin/python git-stats.py --path /Users/cseibert/src/server --days 90 --just-totals" C-m
 
 # return to main vim window
